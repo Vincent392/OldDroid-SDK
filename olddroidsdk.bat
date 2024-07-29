b55f1db5802934081f28c6a514fd9e2a0d14f927
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-07-29 19:45:20 +0100

    Update and rename patchlb3.bat to olddroidsdk.bat

---
+++ olddroidsdk.bat
@@ -0,0 +1,31 @@
+@Echo off
+REM
+REM OldDroid SDK Project Generator
+REM GPL-V3
+REM Patches the game to work with an alternate server
+REM
+goto warning
+:warning
+TITLE OldDroid SDK Project Generator
+echo WARNING: you are about to use an SDK for an ANCIENT version of Android, sure, the SDK is updated, but the OS hasn't been updated since 2017 or so, continue at your own risk.
+PAUSE
+goto main
+:main
+echo =========================
+echo OldDroid SDK
+echo Version 0.50
+echo =========================
+echo What would you like to do?
+echo [1]. Make a Java + XML Project
+echo [2]. Make a Java + HTML/JS Project
+echo [3]. Update SDK base
+echo [4]. Quit
+
+:javaxml
+rem Code for creating a Java + XML Project
+rem Definitely idiotic code
+cls
+echo =========================
+echo OldDroid SDK
+echo Version 0.50
+echo =========================
